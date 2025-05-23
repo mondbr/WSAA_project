48790ed1517b2e705159c33fe40339e517233c3f
mondbr <mondab133@gmail.com>
2025-05-23 21:04:11 +0100

    updated dbconfig

---
+++ dbconfig_pythonanywhere.py
@@ -0,0 +1,8 @@
+# This is a configuration file for connecting to a MySQL database on PythonAnywhere.
+
+dbconfig = {
+   "user": "your_username",
+   "password": "your_password",
+   "host": "your_host", # e.g., 'localhost' for local dev, or the MySQL hostname on PythonAnywhere
+   "database": "your_database",
+}
